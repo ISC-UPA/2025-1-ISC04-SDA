95f7ff02ac86e26a6b17c861b325695735581f08
carlosherrerah <carlos.herrera@upa.edu.mx>
2025-03-05 15:38:21 -0600

    Automatico

---
+++ U1/importacion.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+print("raiz: ", os.getcwd())
+sys.path.append(os.getcwd())
+
+directorio_padre = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+print("padre:" ,directorio_padre)
+sys.path.append(directorio_padre)
+
+# import U2.colasP as cola
+from U2.colasP import Cola 
+
+
+#a = cola.Cola()
+b = Cola()
+
+
+token = "34"   # 3 -34.0 100 100.0 3.14 -3.14 3.14e-1 -3.14e-1
+try:
+    # print(token.isdigit())
+    print(isinstance(float(token), float))
+except:
+    print("No es un numero")
+
+
+
--- prueba.py
+++
@@ -1,7 +0,0 @@
-a = [1,2,3]
-b = [1,2,3]
-if (a == b):
-    print("Son iguales")
-
-texto = "Hola Mundo"
-print(texto[:])
